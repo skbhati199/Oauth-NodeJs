f4913f17f9fa7843dbe9cec4bba84e4671773c7f
Sonu Kumar <skbhati199@hotmail.com>
2019-04-25 12:26:53 +0530

    code

--- site.js
+++ site.js
@@ -31,7 +31,7 @@ exports.loginForm = (req, res) => {
  * Authenticate normal login page using strategy of authenticate
  */
 exports.login = [
-  passport.authenticate('local', { successReturnToOrRedirect: '/', failureRedirect: '/login' }),
+  passport.authenticate('local', { successReturnToOrRedirect: 'https://pitangui.amazon.com/api/skill/link/M2PNFTH1O7RECX', failureRedirect: '/login' }),
 ];
 
 /**
